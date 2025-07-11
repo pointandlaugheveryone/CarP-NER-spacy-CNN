13fceba0cf4261c3ab46589e802e3798e791df1a
pointandlaugheveryone <veronika.havlova@mensagymnazium.cz>
2025-07-11 23:00:40 +0200

    rewrite doc splitting

--- label_models.py
+++ label_models.py
@@ -1,43 +1,45 @@
+import json
 from typing import List
 
 
-class Annotation:
-    id: int
+class Label:
     start: int
     end: int
     label: str
     value: str
 
-    def __init__(self,id:int, start: int, end: int, label: str) -> None:
+    def __init__(self, start: int, end: int, label: str, value:str): 
         self.start = start
-        self.id = id
         self.end = end
         self.label = label
+        self.value = value
 
     def to_dict(self) -> dict:
         return {
-            "id": self.id,
             "start": self.start,
             "end": self.end,
-            "label": self.label
+            "label": self.label,
+            "value":self.value
         }
 
 
 
 class Document:
-    id: int
     content: str
-    annotations: List[Annotation]
-
-    def __init__(self,id:int, content: str, annotations: List[Annotation]) -> None:
-        self.id= id
+    labels: List[Label]
+    source_doc_id: int 
+    '''
+    source_doc_id used for debugging
+    raw data is collected in individual text files, later split into sequences
+    '''
+    def __init__(self,source_doc_id:int, content: str, labels: List[Label]): 
+        self.source_doc_id= source_doc_id
         self.content = content
-        self.annotations = annotations
+        self.labels = labels
 
-    def to_dict(self, annotations:list) -> dict:
+    def to_dict(self) -> dict:
         return {
-            "id":self.id,
+            "source_doc_id":self.source_doc_id,
             "content": self.content,
-            "annotations": annotations,
+            "labels": [a.to_dict() for a in self.labels],
         }
-
